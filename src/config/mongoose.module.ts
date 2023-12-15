2435fef8cee94958de5b942842fafce740d47287
flowermariya <flowermariya811@gmail.com>
2023-12-15 11:39:52 +0530

    FEAT: connected to dev database

--- src/config/mongoose.module.ts
+++ src/config/mongoose.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
 config();
 
-const connectionString = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`;
 console.log('connectionString', connectionString);
 
 @Module({
